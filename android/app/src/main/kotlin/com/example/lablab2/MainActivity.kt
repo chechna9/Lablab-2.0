6bda9c1dd25a122b7dda205aed99cbcb8a60098c
chechna9 <ja_bouchouareb@esi.dz>
2023-08-08 12:17:49 +0100

    init repo

---
+++ android/app/src/main/kotlin/com/example/lablab2/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.lablab2
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+void main() {
+  runApp(const MyApp());
+}
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+   return MaterialApp(
+     title: 'Flutter Demo',
+     debugShowCheckedModeBanner: false,
+     theme: ThemeData(
+       brightness: Brightness.dark,
+     ),
+     home: const HomePage(),
+  );
+  }
+}
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+  @override
+  Widget build(BuildContext context) {
+   return Scaffold(
+     appBar: AppBar(
+       title: const Text(
+        'Flutter Demo',
+         style: TextStyle(
+           color: Colors.white,
+           fontWeight: FontWeight.bold,
+         ),
+       ),
+    ),
+       body: const Center(
+       child: Text('Hello World'),
+     ),
+   );
+  }
+}
